3e94bedee1d354f610d5125884afc0885f66569c
Igor <igormota999@gmail.com>
2024-07-09 16:37:04 +0000

    fix

--- src/data/environment.js
+++ src/data/environment.js
@@ -1,9 +1,6 @@
 export const environment = {
   API_URL: "https://www.talkiebot.com.br/api/v1",
   API_STORE: "https://www.talkiebot.com.br/img/site",
-  //API_URL: "https://shiny-guacamole-pppw55rq796c6jvr-32768.app.github.dev/api/v1",
-  // API_STORE: "hhttps://shiny-guacamole-pppw55rq796c6jvr-32768.app.github.dev/img/site",
-  GOOGLE_CLIENT_ID:
-    "419393571513-6rlisn4b9vl2uni4k6bcuqv8sgsnkrv1.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "419393571513-6rlisn4b9vl2uni4k6bcuqv8sgsnkrv1.apps.googleusercontent.com",
   BASE_URL: "https://crispy-meme-9jj5gg9xpxr3pjgg-3000.app.github.dev",
 };
